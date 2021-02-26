54015abe9cccde3ffbd9468ccbfe99ba6cc6f8a9
halder <sascha.halder91@gmail.com>
2021-02-26 15:54:01 +0100

    Removes sentiment assertion, replace with while-loop

--- label.py
+++ label.py
@@ -7,12 +7,9 @@ def label_rows(data):
         category = input("keyword: ")
         sentiment = int(input("sentiment [-1, 1]: "))
 
-        if sentiment not in (-1, 1):
-            with open("last_index_used.txt", "w") as file:
-                file.write(str(i))
-            file.close()
-
-            raise AssertionError("sentiment has to be either negative (-1) or positive (1)")
+        while sentiment not in (-1 , 1):
+            print("sentiment has to be either negative (-1) or positive (1)")
+            sentiment = int(input("sentiment [-1, 1]: "))
 
         labeled_data.append([data.loc[i].text, category, sentiment])
         print("\n")
